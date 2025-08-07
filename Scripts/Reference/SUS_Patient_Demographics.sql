50846901b37a8ee7933409aca35e01a35ac296c9
Jake Kealey <jake.kealey@nhs.net>
2025-08-07 08:53:11 +0100

    Adjusted field names, added default "Non-NCL" value for null boroughs

--- Scripts/Reference/SUS_Patient_Demographics.sql
+++ Scripts/Reference/SUS_Patient_Demographics.sql
@@ -36,35 +36,35 @@ DEV__MODELLING.CANCER__REF.PATIENTENCOUNTERDATA
 
 --Create and populate the final patient encounter data table with the latest demographic details
 CREATE OR REPLACE DYNAMIC TABLE DEV__MODELLING.CANCER__REF.SUS_PATIENT_DEMOGRAPHICS_NEW(
-    PSEUDO_ID INT,
-    SUS_ENCOUNTER_ID BIGINT,
-    SUS_ENCOUNTER_DATE_TIME DATETIME,
-    SUS_DATA_SOURCE VARCHAR(50),
+    SK_PATIENT_ID INT,
+    SK_ENCOUNTER_ID BIGINT,
+    DATETIME_SUS_ENCOUNTER DATETIME,
+    --SUS_DATA_SOURCE VARCHAR(50),
     GENDER_CODE VARCHAR(50),
     GENDER VARCHAR(50),
     GENDER_LETTER VARCHAR(50),
-    ETHNICITY_SANDPIT_CODE VARCHAR(5),
+    ETHNICITY_SK_CODE VARCHAR(5),
     ETHNICITY_HES_CODE VARCHAR(50),
-    ETHNICITY_HES_CODE_DETAIL VARCHAR(50),
+    ETHNICITY_HES_DETAILED_CODE VARCHAR(50),
     ETHNICITY_CATEGORY VARCHAR(50),
     ETHNICITY_DESC VARCHAR(255),
     ETHNICITY_CATEGORY_AND_DESC VARCHAR(255),
-    DATE_OF_BIRTH DATE,
-    CURRENT_AGE INT,
+    YEAR_OF_BIRTH INT,
+    YEARS_SINCE_BIRTH INT,
     DATE_OF_DEATH DATE,
-    GP_PRACTICE_CODE VARCHAR(50),
-    GP_PRACTICE_NAME VARCHAR(255),
-    GP_PRACTICE_POSTCODE VARCHAR(50),
-    PCN_CODE VARCHAR(50),
-    PCN_NAME VARCHAR(255),
-    ICB_CODE VARCHAR(50),
-    ICB_NAME VARCHAR(255),
-    BOROUGH_NCL VARCHAR(255),
-    LSOA_CODE VARCHAR(50),
-    LSOA_NAME VARCHAR(50),
-    IMD_DECILE_LSOA VARCHAR(50),
-    IMD_QUINTILE_LSOA VARCHAR(50),
-    RUN_DATE_TIME TIMESTAMP_NTZ(9)
+    REG_GP_PRACTICE_CODE VARCHAR(50),
+    REG_GP_PRACTICE_NAME VARCHAR(255),
+    REG_GP_PRACTICE_POSTCODE VARCHAR(50),
+    REG_PCN_CODE VARCHAR(50),
+    REG_PCN_NAME VARCHAR(255),
+    REG_ICB_CODE VARCHAR(50),
+    REG_ICB_NAME VARCHAR(255),
+    REG_BOROUGH VARCHAR(255),
+    RESIDENCE_LSOA_CODE VARCHAR(50),
+    RESIDENCE_LSOA_NAME VARCHAR(50),
+    RESIDENCE_LSOA_IMD_DECILE VARCHAR(50),
+    RESIDENCE_LSOA_IMD_QUINTILE VARCHAR(50),
+    DATETIME_RUN TIMESTAMP_NTZ(9)
 )
 target_lag = '1 DAY'
 refresh_mode = FULL
@@ -172,40 +172,51 @@ CTE_LATEST_RECORDS AS (
 )
 
 SELECT
-    sus."SK_PatientID",
-    sus."SK_EncounterID",
-    sus."EncounterStartDateTime",
-    sus."SUS_DATA_SOURCE",
-    code_gender."GenderCode",
-    code_gender."Gender",
-    code_gender."GenderCode2",
-    sus."SK_EthnicityID",
-    code_ethnic_1."EthnicityHESCode",
-    code_ethnic_1."EthnicityCombinedCode",
-    code_ethnic_2."EthnicityCategory",
-    code_ethnic_2."EthnicityDesc",
-    code_ethnic_1."EthnicityDesc",
-    sus."Date_of_Birth",
+    -- SK ID Fields
+    sus."SK_PatientID" AS SK_PATIENT_ID,
+    sus."SK_EncounterID" AS SK_ENCOUNTER_ID,
+
+    --Datetime of the latest SUS activity
+    sus."EncounterStartDateTime" AS DATETIME_SUS_ENCOUNTER,
+    --sus."SUS_DATA_SOURCE", --Removed as different fields can come from different sources
+
+    --Demographic information
+    code_gender."GenderCode" AS GENDER_CODE,
+    code_gender."Gender" AS GENDER,
+    code_gender."GenderCode2" AS GENDER_LETTER,
+    sus."SK_EthnicityID" AS ETHNICITY_SK_CODE,
+    code_ethnic_1."EthnicityHESCode" AS ETHNICITY_HES_CODE,
+    code_ethnic_1."EthnicityCombinedCode" AS ETHNICITY_HES_DETAILED_CODE,
+    code_ethnic_2."EthnicityCategory" AS ETHNICITY_CATEGORY,
+    code_ethnic_2."EthnicityDesc" AS ETHNICITY_DESC,
+    code_ethnic_1."EthnicityDesc" AS ETHNICITY_CATEGORY_AND_DESC,
+    YEAR(sus."Date_of_Birth") AS YEAR_OF_BIRTH,
     CASE 
         WHEN (sus."Date_of_Birth" IS NOT NULL AND code_death.REG_DATE_OF_DEATH IS NULL) THEN 
         DATEDIFF(YEAR, sus."Date_of_Birth", GETDATE())
         ELSE NULL
-    END AS Current_Age,
-    -- Prefer death registry for date of death
-    code_death.REG_DATE_OF_DEATH AS Date_of_Death,
-    dict_org_gp."Organisation_Code",
-    dict_org_gp."Organisation_Name",
-    dict_post."Postcode_8_chars",
-    dict_org_pcn."Organisation_Code",
-    dict_org_pcn."Organisation_Name",
-    dict_org_healthauth."Organisation_Code",
-    dict_org_healthauth."Organisation_Name",
-    dict_org_borough."Borough",
-    sus."LSOACode",
+    END AS YEARS_SINCE_BIRTH,
     
-    --IMD Deprivation Fields
-    imd.LSOA_NAME_2011,
-    imd.INDEX_OF_MULTIPLE_DEPRIVATION_DECILE,
+    -- Prefer death registry for date of death
+    code_death.REG_DATE_OF_DEATH AS DATE_OF_DEATH,
+
+    -- GP Registration information
+    dict_org_gp."Organisation_Code" AS REG_GP_PRACTICE_CODE,
+    dict_org_gp."Organisation_Name" AS REG_GP_PRACTICE_NAME,
+    dict_post."Postcode_8_chars" AS REG_GP_PRACTICE_POSTCODE,
+    dict_org_pcn."Organisation_Code" AS REG_PCN_CODE,
+    dict_org_pcn."Organisation_Name" AS REG_PCN_NAME,
+    dict_org_healthauth."Organisation_Code" AS REG_ICB_CODE,
+    dict_org_healthauth."Organisation_Name" AS REG_ICB_NAME,
+    CASE 
+        WHEN dict_org_borough."Borough" IS NULL THEN 'Non-NCL'
+        ELSE dict_org_borough."Borough"
+    END AS REG_BOROUGH,
+
+    --Residence information
+    sus."LSOACode" AS RESIDENCE_LSOA_CODE,
+    imd.LSOA_NAME_2011 AS RESIDENCE_LSOA_NAME,
+    imd.INDEX_OF_MULTIPLE_DEPRIVATION_DECILE AS RESIDENCE_LSOA_IMD_DECILE,
     CASE 
         WHEN imd."INDEX_OF_MULTIPLE_DEPRIVATION_DECILE" IN ('1','2') THEN '1 - Most Deprived'
         WHEN imd."INDEX_OF_MULTIPLE_DEPRIVATION_DECILE" IN ('3','4') THEN '2'
@@ -213,8 +224,8 @@ SELECT
         WHEN imd."INDEX_OF_MULTIPLE_DEPRIVATION_DECILE" IN ('7','8') THEN '4'
         WHEN imd."INDEX_OF_MULTIPLE_DEPRIVATION_DECILE" IN ('9','10') THEN '5 - Least Deprived'
         ELSE 'Unknown' 
-    END,
-    GETDATE() AS RUN_DATE_TIME
+    END AS RESIDENCE_LSOA_IMD_QUINTILE,
+    GETDATE() AS DATETIME_RUN
     
 FROM CTE_LATEST_RECORDS sus
 
