ec7df127197d773bf22dd2b097469ebe135aca65
Jake Kealey <jake.kealey@nhs.net>
2025-09-08 14:29:41 +0100

    Repointed to DATA_LAKE__NCL

--- Scripts/Primary_Care/EMIS/Silver_CCR_and_eSafety.sql
+++ Scripts/Primary_Care/EMIS/Silver_CCR_and_eSafety.sql
@@ -1,7 +1,7 @@
 -- This pulls raw data on eSafety Netting and Cancer Care Review metrics and combines them into a single table
 -- Contact: jake.kealey@nhs.net
 
-CREATE OR REPLACE DYNAMIC TABLE DEV__MODELLING.CANCER__PRIMARY_CARE.SAFETY_NETTING_AND_CCR(
+CREATE OR REPLACE DYNAMIC TABLE DEV__MODELLING.CANCER__EMIS.SAFETY_NETTING_AND_CCR(
     "INDICATOR_NAME",
     "PRACTICE_NAME",
     "PRACTICE_CODE",
@@ -28,15 +28,15 @@ SELECT
 --For each data source, add a "Indicator_Name" and combine the datasets
 FROM (
     SELECT 'CAN004 - Cancer Care Review within 12 months' AS "Indicator_Name", * 
-    FROM DATA_LAKE.CANCER__EMIS.CCR_CAN004
+    FROM DATA_LAKE__NCL.CANCER__EMIS.CCR_CAN004
     
     UNION ALL
     
     SELECT 'CAN005 - Cancer support offered within 3 months' AS "Indicator_Name", * 
-    FROM DATA_LAKE.CANCER__EMIS.CCR_CAN005
+    FROM DATA_LAKE__NCL.CANCER__EMIS.CCR_CAN005
     
     UNION ALL
     
     SELECT 'USC referrals safety netted via e-safety netting tool' AS "Indicator_Name", * 
-    FROM DATA_LAKE.CANCER__EMIS.ESAFETY
+    FROM DATA_LAKE__NCL.CANCER__EMIS.ESAFETY
 )
